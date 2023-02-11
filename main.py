a312dfd83a205a939faefca178e35cdae47bb559
paulo <paulolsouza19@gmail.com>
2023-02-11 20:02:24 -0300

    Implemented linear search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from utils import *
 
 criar_base_de_dados("teste.txt")
+print(busca_sequencial("teste", 1))
--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import random
+import time
 from funcionario import Funcionario
 
 def criar_base_de_dados(nome_arquivo: str):
   try:
-    id_list = [i for i in range(5000)]
+    id_list = [i for i in range(10)]
     arq = open(nome_arquivo, "w")
     i = 0
-    while i < 5000:
+    
+    while i < 10:
       func = Funcionario(id = random.choice(id_list))
       id_list.remove(func.id)
       arq.write(str(func.id))
@@ -24,4 +26,27 @@ def criar_base_de_dados(nome_arquivo: str):
     print(IOError)
     exit(1)
   print("Base de dados criada")
+  
   arq.close()
+
+def busca_sequencial(nome_arquivo: str, chave: int):
+  tempo_inicial = time.perf_counter()
+  arq = open(nome_arquivo + ".txt", "r")
+  registros = arq.readlines() 
+  
+  for i in range(len(registros)):
+    registro = registros[i]
+    codigo = ""    
+    for j in range(len(registro)):
+      if registro[j] == "|":
+        if codigo == str(chave):
+          arq.close()
+          tempo_final = time.perf_counter() - tempo_inicial
+          return registro, i+1, tempo_final
+        else:
+          break 
+      codigo += registro[j]
+  
+  arq.close()
+
+
